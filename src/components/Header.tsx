4a06701034e60d117baf6240a1c7dad03d5b3b14
Abdelrahman Hamza <a.m.hamza156@gmail.com>
2024-10-11 21:53:21 +0300

    change break point to md instead of lg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,18 +27,18 @@ export default function Home() {
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 relative">
+        <nav className="bg-white border-gray-200 px-4 md:px-6 py-2.5 relative">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
               <EnglishLogo />
             </Link>
 
-            <div className="flex items-center lg:order-2">
-              <div className="hidden lg:flex lg:items-center">
+            <div className="flex items-center md:order-2">
+              <div className="hidden md:flex md:items-center">
                 <TrackRequest />
                 <Link
                   to="/"
-                  className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none font-semibold"
+                  className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-4 md:px-5 py-2 md:py-2.5 mr-2 focus:outline-none font-semibold"
                 >
                   {t("Login")}
                 </Link>
@@ -48,7 +48,7 @@ export default function Home() {
               </Button>
               <Button
                 variant="transparent"
-                className="lg:hidden"
+                className="md:hidden"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
               >
                 <span className="sr-only">Open main menu</span>
@@ -58,27 +58,27 @@ export default function Home() {
             <div
               className={`${
                 isMenuOpen ? "block" : "hidden"
-              } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
+              } justify-between items-center w-full md:flex md:w-auto md:order-1`}
               id="mobile-menu-2"
             >
-              <ul className="flex flex-col font-medium lg:flex-row lg:mt-0 gap-5 m-0">
+              <ul className="flex flex-col font-medium md:flex-row md:mt-0 gap-5 m-0">
                 {menuItems.map((item) => (
                   <li key={item.title}>
                     <Link
                       to={item.to}
-                      className="block text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 font-semibold"
+                      className="block text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 font-semibold"
                     >
                       {item.title}
                     </Link>
                   </li>
                 ))}
-                <li className="lg:hidden">
+                <li className="md:hidden">
                   <TrackRequest />
                 </li>
-                <li className="lg:hidden">
+                <li className="md:hidden">
                   <Link
                     to="/"
-                    className="block text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 font-semibold"
+                    className="block text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 font-semibold"
                   >
                     {t("Login")}
                   </Link>
